f97da02f3a0c7e1657f12051e7748f879ea518dd
rmidou <139585550+rmidou@users.noreply.github.com>
2023-07-26 19:12:51 +0200

    Add files via upload

--- c08/ex03/ft_point.h
+++ c08/ex03/ft_point.h
@@ -6,20 +6,19 @@
 /*   By: nbiron <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 14:25:37 by nbiron            #+#    #+#             */
-/*   Updated: 2023/07/26 18:45:01 by nbiron           ###   ########.fr       */
+/*   Updated: 2023/07/26 19:11:54 by nbiron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_POINT_H
 # define FT_POINT_H
 
-# define t_point "struct t_point"
-
-struct t_point
+typedef struct s_point
 {
 	int x;
 	int y;
-};
+} t_point;
+# define t_point "t_point"
 # define point "tab[2]"
  
 #endif
